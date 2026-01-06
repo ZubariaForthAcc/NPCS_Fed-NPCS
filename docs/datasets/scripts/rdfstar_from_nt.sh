0c3fa2bfea111d65ad4902a5961d3d385bb8e160
Zubaria Asma <158457860+ZubariaForthAcc@users.noreply.github.com>
2026-01-06 14:25:20 +0200

    Update rdfstar_from_nt.sh

--- docs/datasets/scripts/rdfstar_from_nt.sh
+++ docs/datasets/scripts/rdfstar_from_nt.sh
@@ -1,1 +1,16 @@
 
+
+```bash
+#!/bin/bash
+INPUT=$1
+OUTPUT=$2
+
+gzip "$INPUT"
+
+zcat "$INPUT.gz" | \
+awk '{print " ", $0, "{|   <http://example.org/occurrenceOf>     <http://example.org/statement/" NR "> |}  ."}' | \
+sed -e 's/\t/ /g' -e 's/. {|/{|/' | \
+gzip > "$OUTPUT.gz"
+
+gunzip "$OUTPUT.gz"
+sed -i 's/\.{|/{|/g' "$OUTPUT"
